ffad50c2d2745ada2181cae4dee46fa180821488
camtbing <46546713+camtbing@users.noreply.github.com>
2020-02-24 15:21:40 -0700

    Workout entry front end attempt 1

---
+++ CoachTim_1/CoachTim_1/Controllers/WorkoutsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CoachTim_1.Models;
+
+namespace CoachTim_1.Controllers
+{
+    public class WorkoutsController : Controller
+    {
+        private Models.DbContext db = new Models.DbContext();
+
+        // GET: Workouts
+        public ActionResult Index()
+        {
+            return View(db.Workouts.ToList());
+        }
+
+        // GET: Workouts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Workout workout = db.Workouts.Find(id);
+            if (workout == null)
+            {
+                return HttpNotFound();
+            }
+            return View(workout);
+        }
+
+        // GET: Workouts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Workouts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "WorkoutId,AthleteId,WorkoutType,WorkoutDate,WorkoutDistance,WorkoutHours,WorkoutMinutes,WorkoutSeconds,AverageHeartRate,PreviousNightSleepHours,PreviousNightSleepMinutes")] Workout workout)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Workouts.Add(workout);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(workout);
+        }
+
+        // GET: Workouts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Workout workout = db.Workouts.Find(id);
+            if (workout == null)
+            {
+                return HttpNotFound();
+            }
+            return View(workout);
+        }
+
+        // POST: Workouts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "WorkoutId,AthleteId,WorkoutType,WorkoutDate,WorkoutDistance,WorkoutHours,WorkoutMinutes,WorkoutSeconds,AverageHeartRate,PreviousNightSleepHours,PreviousNightSleepMinutes")] Workout workout)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(workout).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(workout);
+        }
+
+        // GET: Workouts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Workout workout = db.Workouts.Find(id);
+            if (workout == null)
+            {
+                return HttpNotFound();
+            }
+            return View(workout);
+        }
+
+        // POST: Workouts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Workout workout = db.Workouts.Find(id);
+            db.Workouts.Remove(workout);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CoachTim_1/CoachTim_1/Models/Workout.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoachTim_1.Models
+{
+    public class Workout
+    {
+        public int WorkoutId { get; set; }
+        public int AthleteId { get; set; }
+        public string WorkoutType { get; set; }
+        public DateTime WorkoutDate { get; set; }
+        public int WorkoutDistance { get; set; }
+        public int WorkoutHours { get; set; }
+        public int WorkoutMinutes { get; set; }
+        public int WorkoutSeconds { get; set; }
+        public int AverageHeartRate { get; set; }
+        public int PreviousNightSleepHours { get; set; }
+        public int PreviousNightSleepMinutes { get; set; }
+
+    }
+}
